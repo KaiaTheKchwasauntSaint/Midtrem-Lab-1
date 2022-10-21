79ac662cd600785caf3926d06e1e5634125506f4
#ProcrastinationQueen <116268462+kaiaWANTSTOBEATUTUTBI@users.noreply.github.com>
2022-10-21 17:48:00 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buenafe__mid__event_lab_1
+{
+    class StudentInformationClass
+    {
+        public static int SetStudentNo = 0;
+        public static long SetContactNo = 0;
+        public static int SetAge = 0;
+        public static string SetProgram = "";
+        public static string SetGender = "";
+        public static string SetBirthday = "";
+        public static string SetFullName = "";
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Buenafe__mid__event_lab_1
+{
+    public partial class frmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public long StudentNumber(string studNum)
+        {
+            try {
+                _StudentNo = long.Parse(studNum);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Format Exception Message: " + fe.Message);
+            }
+            return _StudentNo; 
+            
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            } else
+            {
+
+            }
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            } else
+            {
+                    MessageBox.Show("Invalid Input");
+            }
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input");
+           
+            }
+
+            return _Age;
+        }
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]
+            {
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS in Tourism Management"
+            };
+            for(int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+            StudentInformationClass.SetStudentNo = Convert.ToInt32(StudentNumber(txtStudentNo.Text));
+            StudentInformationClass.SetProgram = cbPrograms.Text;
+            StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+            StudentInformationClass.SetGender = cbGender.Text;
+            StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+            StudentInformationClass.SetAge = Convert.ToInt32(Age(txtAge.Text));
+           
+
+
+            frmConfirmation frm = new frmConfirmation();
+            frm.ShowDialog();
+
+            txtLastName.Clear();
+            txtFirstName.Clear();
+            txtMiddleInitial.Clear();
+            txtStudentNo.Clear();
+            txtContactNo.Clear();
+            txtAge.Clear();
+            
+
+
+
+        }
+    }
+}
